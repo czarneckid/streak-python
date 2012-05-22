3224dbdd4446f5e57ed7a7e2490d987afd63a7fd
David Czarnecki <czarneckid@acm.org>
2012-05-22 15:45:16 -0400

    Initial port of Ruby streak library to Python

--- tests/streak_test_case.py
+++ tests/streak_test_case.py
@@ -2,6 +2,53 @@ import os
 import redis
 import unittest
 
+from streak import Streak
+
 class StreakTestCase(unittest.TestCase):
-  def test_truth(self):
-    self.assertEquals(True, True)
+
+  def setUp(self):
+    self.streak = Streak()
+    redis = self.streak.redis
+    redis.flushdb()
+
+  def test_streak_default_values(self):
+    self.assertEquals('streak', Streak.DEFAULTS['namespace'])
+    self.assertEquals('wins', Streak.DEFAULTS['positive_key'])
+    self.assertEquals('wins_total', Streak.DEFAULTS['positive_total_key'])
+    self.assertEquals('wins_streak', Streak.DEFAULTS['positive_streak_key'])
+    self.assertEquals('losses', Streak.DEFAULTS['negative_key'])
+    self.assertEquals('losses_total', Streak.DEFAULTS['negative_total_key'])
+    self.assertEquals('losses_streak', Streak.DEFAULTS['negative_streak_key'])
+    self.assertEquals('total', Streak.DEFAULTS['total_key'])
+
+  def test_aggregate_statistics_and_reset_statistics(self):
+    streak = Streak()
+    streak.aggregate('david', 3)
+    streak.aggregate('david', -2)
+    streak.aggregate('david', 5)
+    streak.aggregate('david', -1)
+    
+    statistics = streak.statistics('david')
+    self.assertEquals(7, len(statistics))
+    self.assertEquals(0, statistics['wins'])
+    self.assertEquals(8, statistics['wins_total'])
+    self.assertEquals(5, statistics['wins_streak'])
+    self.assertEquals(1, statistics['losses'])
+    self.assertEquals(3, statistics['losses_total'])
+    self.assertEquals(2, statistics['losses_streak'])
+    self.assertEquals(11, statistics['total'])
+
+    statistics = streak.statistics('david', [Streak.DEFAULTS['positive_streak_key'], Streak.DEFAULTS['negative_streak_key']])
+    self.assertEquals(2, len(statistics))
+    self.assertEquals(5, statistics['wins_streak'])
+    self.assertEquals(2, statistics['losses_streak'])
+
+    streak.reset_statistics('david')
+    statistics = streak.statistics('david')
+    self.assertEquals(0, statistics['wins'])
+    self.assertEquals(0, statistics['wins_total'])
+    self.assertEquals(0, statistics['wins_streak'])
+    self.assertEquals(0, statistics['losses'])
+    self.assertEquals(0, statistics['losses_total'])
+    self.assertEquals(0, statistics['losses_streak'])
+    self.assertEquals(0, statistics['total'])
