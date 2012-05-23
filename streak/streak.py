d73a1407bdb7a3947e68adcfca28ab155b3c6aea
David Czarnecki <czarneckid@acm.org>
2012-05-22 22:23:26 -0400

    Internal code documentation

--- streak/streak.py
+++ streak/streak.py
@@ -18,6 +18,19 @@ class Streak(object):
     self.redis = redis.StrictRedis(host = 'localhost', port = 6379, db = 0)
 
   def aggregate(self, id, count):
+    '''
+    Aggregate streaks for a given +id+. If +count+ is greater than 0, it will increment +Streak.positive_key+ and 
+    +Streak.positive_total_key+ by the absolute value of count. It will zero out +Streak.negative_key+. Finally, it 
+    will add the absolute value of count to +Streak.total_key+. If the current positive streak is greater than 
+    the value of +Streak.positive_streak_key+, its value will be replaced. 
+    If +count+ is less than than 0, it will increment +Streak.negative_key+ and 
+    +Streak.negative_total_key+ by the absolute value of count. It will zero out +Streak.positive_key+. Finally, it 
+    will add the absolute value of count to +Streak.total_key+. If the current negative streak is greater than 
+    the value of +Streak.negative_streak_key+, its value will be replaced.
+    
+    @param id [String] ID of the item being monitored for a streak.
+    @param count [Integer] Streak count, which can be positive or negative.
+    '''
     if count >= 0:
       pipe = self.redis.pipeline()
       pipe.get('%s:%s:%s' \
@@ -84,6 +97,18 @@ class Streak(object):
       pipe.execute()
 
   def statistics(self, id, keys = []):
+    '''
+    Retrieve all (or some) of the streak statistics collected. By default, without a second parameter, this 
+    method will return a +dict+ of: +Streak.positive_key+, +Streak.positive_total_key+, +Streak.positive_streak_key+, 
+    +Streak.negative_key+, +Streak.negative_total_key+, +Streak.negative_streak_key+, and +Streak.total_key+ with
+    their corresponding values. If you want a subset of that list, pass in an array with the keys you want
+    returned.
+    
+    @param id [String] ID.
+    @param keys [Array, optional]. Optional list of streak statistic keys to be retrieved.
+    
+    @return +dict+ of streak statistics and their corresponding values for a given +id+.
+    '''
     if len(keys) == 0:
       keys = self.slicedict(self.options, 'namespace').values()
 
@@ -99,6 +124,11 @@ class Streak(object):
     return dict(zip(keys, map(int, values)))
 
   def reset_statistics(self, id):
+    '''
+    Reset all the statistics for a given +id+ to 0.
+    
+    @param id [String] ID.
+    '''
     keys = keys = self.slicedict(self.options, 'namespace').values()
 
     pipe = self.redis.pipeline()
